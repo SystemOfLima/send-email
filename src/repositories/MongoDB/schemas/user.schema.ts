82af274d3698caf9fba7ca498b6b54ad7a7e6d4f
João Antonio <hiukakamachi@gmail.com>
2023-01-22 18:56:07 -0300

    Add hashPassword

--- src/repositories/MongoDB/schemas/user.schema.ts
+++ src/repositories/MongoDB/schemas/user.schema.ts
@@ -1,7 +1,29 @@
 import { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const UserSchema = new Schema({
   name: String,
   email: String,
   password: String,
 });
+
+UserSchema.pre("save", async function hashPassword(next) {
+  if (!this.isModified("password")) next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+UserSchema.methods = {
+  compareHash(hash: string) {
+    return bcrypt.compare(hash, this.password);
+  },
+};
+
+UserSchema.statics = {
+  generateToken({ id }) {
+    return jwt.sign({ id }, process.env.KEY_TOKEN, {
+      expiresIn: 86400,
+    });
+  },
+};
